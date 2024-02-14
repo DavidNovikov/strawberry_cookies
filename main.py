d944329cf075aa28ecc4c8a937ea1ceecaa6e152
DavidNovikov <dn9678@gmail.com>
2024-02-14 11:49:12 +0200

    buggy code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 from torch import optim
 
-from models import encoder_decoder_net
+from models import encoder_decoder_net_notes_first
 from train import train
 from test import test
 from config import get_cfg
@@ -8,28 +9,24 @@ from data_handler import data_set_split
 
 
 def create_train_param(cfg, train_data_loader, valid_data_loader):
-    #(f, f_copy, opt, data_loader, n_epochs)
+    # (f, f_copy, opt, data_loader, n_epochs)
     n_epochs = cfg['n_epochs']
-    f = encoder_decoder_net(1)
-    f_copy = encoder_decoder_net(1)
+    f = encoder_decoder_net_notes_first()
+    f_copy = encoder_decoder_net_notes_first()
     opt = optim.Adam(f.parameters(), lr=cfg['lr'])
     return f, f_copy, opt, train_data_loader, valid_data_loader, n_epochs
 
 
-
-
 def main_learn():
     cfg = get_cfg()
+    print(f'{os.getcwd()}/png_files/')
     data_loader_train, data_loader_valid, data_loader_test = data_set_split(
-        "C:\\Users\\97254\\Desktop\\Masters-semester-A\\Deep_Learning_for_Computer_Vision_Fundamentals_and_Applications\\final_project\\code\\png_files\\",
-    batch_size = 2)
+        f'{os.getcwd()}/png_files/',
+        batch_size=2)
     param = create_train_param(cfg, data_loader_train, data_loader_valid)
     train(*param)
     test(param[1], data_loader_test)
 
 
-
 if __name__ == '__main__':
     main_learn()
-
-
--- models.py
+++ models.py
@@ -2,25 +2,23 @@
 from torch import nn
 
 
-class encoder_decoder_net(nn.Module):
-    # under the assumtion that we start with 1X16X16
-    def __init__(self, input_dim):
-        super(encoder_decoder_net, self).__init__()
+class encoder_decoder_net_notes_first(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
-        # encoder
-        self.conv1 = nn.Conv2d(input_dim, input_dim*2, kernel_size=4, stride=4)
-        self.conv2 = nn.Conv2d(input_dim*2, input_dim * 4, kernel_size=4, stride=4)
-        self.conv3 = nn.Conv2d(input_dim * 4, input_dim * 8, kernel_size=4, stride=2)
-        self.conv4 = nn.Conv2d(input_dim * 8, input_dim * 16, kernel_size=4, stride=2)
+        self.conv1 = nn.Conv2d(1, 2, kernel_size=(22, 1), stride=(2, 1))
+        self.conv2 = nn.Conv2d(2, 4, kernel_size=(4, 10), stride=2)
+        self.conv3 = nn.Conv2d(4, 8, kernel_size=(4, 12), stride=4)
+        self.conv4 = nn.Conv2d(8, 16, kernel_size=(4, 6), stride=2)
 
         # decoder
-        self.deconv1 = nn.ConvTranspose2d(input_dim*16, input_dim*8, kernel_size=4, stride=2)
-        self.deconv2 = nn.ConvTranspose2d(input_dim*8, input_dim * 4, kernel_size=4, stride=2)
-        self.deconv3 = nn.ConvTranspose2d(input_dim * 4, input_dim * 2, kernel_size=4, stride=4)
-        self.deconv4 = nn.ConvTranspose2d(input_dim * 2, input_dim, kernel_size=4, stride=4)
+        self.deconv1 = nn.ConvTranspose2d(16, 8, kernel_size=(4, 6), stride=2)
+        self.deconv2 = nn.ConvTranspose2d(8, 4, kernel_size=(4, 12), stride=4)
+        self.deconv3 = nn.ConvTranspose2d(4, 2, kernel_size=(4, 10), stride=4)
+        self.deconv4 = nn.ConvTranspose2d(
+            2, 1, kernel_size=(22, 1), stride=(2, 1))
 
         self.relu = nn.ReLU()
-
 
     def forward(self, x):
         # Encoder
@@ -36,3 +34,41 @@ class encoder_decoder_net(nn.Module):
         return x
 
 
+# class encoder_decoder_net(nn.Module):
+#     # under the assumtion that we start with 1X16X16
+#     def __init__(self, input_dim):
+#         super(encoder_decoder_net, self).__init__()
+
+#         # encoder
+#         self.conv1 = nn.Conv2d(input_dim, input_dim*2, kernel_size=4, stride=4)
+#         self.conv2 = nn.Conv2d(input_dim*2, input_dim *
+#                                4, kernel_size=4, stride=4)
+#         self.conv3 = nn.Conv2d(input_dim * 4, input_dim *
+#                                8, kernel_size=4, stride=2)
+#         self.conv4 = nn.Conv2d(input_dim * 8, input_dim *
+#                                16, kernel_size=4, stride=2)
+
+#         # decoder
+#         self.deconv1 = nn.ConvTranspose2d(
+#             input_dim*16, input_dim*8, kernel_size=4, stride=2)
+#         self.deconv2 = nn.ConvTranspose2d(
+#             input_dim*8, input_dim * 4, kernel_size=4, stride=2)
+#         self.deconv3 = nn.ConvTranspose2d(
+#             input_dim * 4, input_dim * 2, kernel_size=4, stride=4)
+#         self.deconv4 = nn.ConvTranspose2d(
+#             input_dim * 2, input_dim, kernel_size=4, stride=4)
+
+#         self.relu = nn.ReLU()
+
+#     def forward(self, x):
+#         # Encoder
+#         x = self.relu(self.conv1(x))
+#         x = self.relu(self.conv2(x))
+#         x = self.relu(self.conv3(x))
+#         x = self.relu(self.conv4(x))
+#         # Decoder
+#         x = self.relu(self.deconv1(x))
+#         x = self.relu(self.deconv2(x))
+#         x = self.relu(self.deconv3(x))
+#         x = self.relu(self.deconv4(x))
+#         return x
