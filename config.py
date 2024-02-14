6e3e560540da0c62f9083af940ab29a89e859ead
DavidNovikov <dn9678@gmail.com>
2024-02-14 18:50:10 +0200

    cuda adjustment

--- config.py
+++ config.py
@@ -4,10 +4,12 @@ import os
 
 
 def get_cfg():
+    os_type = os.name
     dct = {
         'lr':  0.001,
         'n_epochs': 5,
-        'device': 'mps' if torch.backends.mps.is_built() else 'cuda' if torch.cuda.is_available() else 'cpu',
+        # 'device': 'mps' if torch.backends.mps.is_built() else 'cuda' if torch.cuda.is_available() else 'cpu',
+        'device': 'cuda' if torch.cuda.is_available() and os_type != 'nt' else 'cpu',
         'os': os.name
     }
 
